61c7ac1c401b2ebf233b3d5f23804434cad1f89f
LukeDaniel16 <lucasfelepe@gmail.com>
2021-10-02 23:01:32 -0300

    Removendo um pouco da corrupção de código e brechas.

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Lucas", "Silva", "15725748714", "luke@luke.luke");
+            student.AddSubscription(subscription);
+        }
+    }
+}
